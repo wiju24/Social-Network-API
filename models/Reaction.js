bc7839fcb497c4ca8b919296185e16b96df8b096
Vijithiran Navajeevayokan <vijithiran_navajeevayokan@hotmail.com>
2023-01-28 12:36:18 -0500

    Created the model routes for the Reaction, Thought and Users while connecting them to the mongoose database.

---
+++ models/Reaction.js
@@ -0,0 +1,37 @@
+const {Schema, Types} = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema ({
+    reactionId: 
+    {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody:
+    {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username:
+    {
+        type: String,
+        required: true,
+    },
+    createAt:
+    {
+        type: Date,
+        default: Date.now,
+        get: (displayDate) => moment(displayDate).format('MMM-DD-YYYY [at] HH:mm')
+    }
+},
+    {
+        toJSON:
+        {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
